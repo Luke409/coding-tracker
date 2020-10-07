b56ab3c18457356c72590e0faaafa32e80a58f07
luke409 <lukeblack09@gmail.com>
2020-10-06 23:29:26 -0500

    Adding simple form

---
+++ coding-tracker/src/ClockInForm.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import logo from './logo.svg';
+//import './App.css';
+
+function ClockInForm() {
+  
+    // handleSubmit(event){
+    //     console.log(
+
+    //     )
+    // }
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  return (
+    <form>  
+        <h1>Log your coding activity here</h1>
+        <div>
+            Task:
+            <input type="text">
+            </input>
+        </div>
+        <div>
+            <button>Submit</button>
+            {/* <button>Clock out</button> */}
+        </div>     
+        
+    </form>
+  );
+}
+
+export default ClockInForm;
